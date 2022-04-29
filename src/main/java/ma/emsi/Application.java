76375d1795f6534961a430cebcf78627cdbe606a
Fahd KORAICHE <roshanfahd@gmail.com>
2022-04-29 10:07:38 +0000

    add comments

--- src/main/java/ma/emsi/Application.java
+++ src/main/java/ma/emsi/Application.java
@@ -1,7 +1,10 @@
 package ma.emsi;
-
+/*
+    this is a comment in the new branch
+*/
 public class Application{
     public static void main(String[] args){
+        // This is another comment
         System.out.println("Hello world!");
     }
 }
